980a085a0091bf252eb855715b1d929469132d0e
Anthony Fernandez <anthony.fernandez2021@campus-eni.fr>
2021-09-14 15:35:59 +0200

    Change : Forms sortie

--- migrations/Version20210914102146.php
+++ migrations/Version20210914102146.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20210914083320 extends AbstractMigration
+final class Version20210914102146 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,7 +20,7 @@ final class Version20210914083320 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE sortie ADD site_id INT NOT NULL');
+        $this->addSql('ALTER TABLE sortie CHANGE site_id site_id INT DEFAULT NULL');
         $this->addSql('ALTER TABLE sortie ADD CONSTRAINT FK_3C3FD3F2F6BD1646 FOREIGN KEY (site_id) REFERENCES site (id)');
         $this->addSql('CREATE INDEX IDX_3C3FD3F2F6BD1646 ON sortie (site_id)');
     }
@@ -30,6 +30,6 @@ final class Version20210914083320 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE sortie DROP FOREIGN KEY FK_3C3FD3F2F6BD1646');
         $this->addSql('DROP INDEX IDX_3C3FD3F2F6BD1646 ON sortie');
-        $this->addSql('ALTER TABLE sortie DROP site_id');
+        $this->addSql('ALTER TABLE sortie CHANGE site_id site_id INT NOT NULL');
     }
 }

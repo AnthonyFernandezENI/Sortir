43760e44da57502fcbb32b3fbef32962132cf0c8
Aurélien Samson <aurelien2.samson@gmail.com>
2021-09-16 16:52:48 +0200

    ADD #1 : création user admin par défaut pour premier user

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,10 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\Participant;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use function PHPUnit\Framework\isEmpty;
 
 class SecurityController extends AbstractController
 {
@@ -17,13 +19,20 @@ class SecurityController extends AbstractController
         // if ($this->getUser()) {
         //     return $this->redirectToRoute('target_path');
         // }
-
-        // get the login error if there is one
-        $error = $authenticationUtils->getLastAuthenticationError();
-        // last username entered by the user
-        $lastUsername = $authenticationUtils->getLastUsername();
-
-        return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+        $repo = $this->getDoctrine()->getRepository(Participant::class);
+        if (!$this->isGranted('IS_AUTHENTICATED_FULLY')) {
+            if(empty($repo->findAll())){
+                return $this->redirectToRoute('profil_new');
+            } else {
+                // get the login error if there is one
+                $error = $authenticationUtils->getLastAuthenticationError();
+                // last username entered by the user
+                $lastUsername = $authenticationUtils->getLastUsername();
+            }
+            return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+        } else {
+            return $this->redirectToRoute('sortie_index');
+        }
     }
 
     /**
